6bf2ab5929a65623403ff6e43d51603bcfb905db
Prasad-045 <gorigeprasad8@gmail.com>
2025-05-13 18:39:13 +0530

    Fixed responsive navbar and other updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Includes Popper too
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
